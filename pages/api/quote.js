f3e57a09a601f881df56ee9a2eae61e458535b38
sukeub-collab <sukeub@gmail.com>
2026-03-20 13:02:31 +0100

    Update quote.js

--- pages/api/quote.js
+++ pages/api/quote.js
@@ -37,7 +37,4 @@ export default async function handler(req, res) {
   } catch(e) {
     return res.status(500).json({ error: e.message });
   }
-}  } catch(e) {
-    return res.status(500).json({ error: e.message });
-  }
-}
+  
